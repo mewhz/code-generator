190c7689598d7eb2e0eb566ca564aa36b429bb13
mewhz <mewanghz@163.com>
2025-05-30 18:08:31 +0800

    更新 1.3.1 版本 详见更新日志

--- generator/src/main/java/com/mewhz/generator/model/GeneratorConfig.java
+++ generator/src/main/java/com/mewhz/generator/model/GeneratorConfig.java
@@ -7,6 +7,6 @@ public class GeneratorConfig {
     private String projectName;  // 添加项目名字段
     private Boolean enableLombok;
     private Boolean enableKnife4j;
-    private Boolean enableValidation;
+//    private Boolean enableValidation;
     private Boolean enableResult;
 } 
